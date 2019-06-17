1f0c25c62e916baad5c51e480251749696c970d5
shishutong <shishutong49@gmail.com>
2019-06-17 15:48:45 +0800

    js开始学

---
+++ javascript/loop.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Gao Chengdong 
+ * @Date: 2019-06-17 15:45:04 
+ * @Last Modified by: Gao Chengdong
+ * @Last Modified time: 2019-06-17 15:46:13
+ */
+
+var x = 0;
+for(var i=0; i<=10000; i++){
+    x = x+i;
+}
+console.log(x);
